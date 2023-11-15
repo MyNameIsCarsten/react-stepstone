a12d5d84eb1d6841fe3a8f4f26bd3e5be318d7ab
Carsten Linder <Carsten@Carsten-Linder.de>
2023-11-15 22:34:34 +0100

    Navbar with popups

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import styled from 'styled-components';
 import './App.css';
+import Navbar from './components/navbar/Navbar';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+const HeaderWrapper = styled.header`
+  background: url('./background.svg');
+  background-position: top center;
+  background-attachment: scroll;
+  background-repeat: no-repeat;
+  background-size: 100% 100%;
+  padding: 1em 1.5em;
+  /* Additional styles for the header if needed */
+`;
+
+function Home() {
+  return (
+    <>
+      <HeaderWrapper>
+        <Navbar />
+      </HeaderWrapper>
+      <p>Hello World!</p>
+    </>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* Add more routes as needed */}
+        </Routes>
+      </Router>
     </div>
   );
 }
