6bfd25deed3e550ffc45c8436dd1efa7b5e1bc18
Carsten Linder <Carsten@Carsten-Linder.de>
2023-11-20 16:12:01 +0100

    Filters can now be expanded and collapsed

--- src/components/filtercard/FilterCard.tsx
+++ src/components/filtercard/FilterCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 
 const CardWrapper = styled.div`
@@ -13,6 +13,8 @@ const CardWrapper = styled.div`
     margin-bottom: 16px;
 `;
 
+
+
 const TopicWrapper = styled.div`
     font-weight: bold;
     padding: 0.7em 0 0.7em  16px ;
@@ -21,15 +23,30 @@ const TopicWrapper = styled.div`
     align-items: center;
 `;
 
+interface OptionsWrapperProps {
+  $isVisible?: boolean;
+}
+
+const OptionsWrapper = styled.div<OptionsWrapperProps>`
+  display: ${props => props.$isVisible ? 'none' : 'flex'};
+  flex-direction: column;
+  @media (max-width: 900px){
+    display: ${props => props.$isVisible ? 'flex' : 'none'};
+  }
+`;
+
 const FilterWrapper = styled.div`
     border-top: 2px solid #CFD6E7;
     padding: 0.7em 0 0.7em  32px ;
 `;
 
-const StyledSvg = styled.svg`
+const StyledSvg = styled.svg<OptionsWrapperProps>`
     max-width: 2em;
-    max-height: 2em
-    transform: rotate(180deg);
+    max-height: 2em;
+    transform: ${props => props.$isVisible ? 'rotate(0deg)' : 'rotate(180deg)'};
+    @media (max-width: 900px){
+      transform: ${props => props.$isVisible ? 'rotate(180deg)' : 'rotate(0deg)'};
+    }
   `;
 
 const StyledButton = styled.button`
@@ -37,6 +54,9 @@ const StyledButton = styled.button`
   background: none;
   color: rgb(12, 37, 119);
   display: flex;
+  &:hover {
+    cursor: pointer;
+  }
 `;
 
 type Props = {
@@ -45,15 +65,20 @@ type Props = {
 }
 
 const FilterCard: React.FC<Props> = ({topic, filters}) => {
+
+  const [expanded, setExpanded] = useState<boolean>(true);
+
   return (
     <CardWrapper>
       <TopicWrapper>
         {topic}
-        <StyledButton>
-            <StyledSvg data-genesis-element="ChevronBottomIcon" aria-hidden="true" role="presentation" xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="none" viewBox="0 0 20 20"><path fill="currentColor" d="M10 12.208a.864.864 0 0 1-.292-.052.64.64 0 0 1-.27-.198L5.75 8.292a.668.668 0 0 1-.177-.49.672.672 0 0 1 .198-.469c.153-.139.312-.205.48-.198a.72.72 0 0 1 .457.198L10 10.646l3.312-3.292a.574.574 0 0 1 .438-.208c.167 0 .326.07.48.208a.66.66 0 0 1 .208.479c0 .18-.07.334-.209.459l-3.667 3.666a.64.64 0 0 1-.27.198.864.864 0 0 1-.292.052Z"></path></StyledSvg>
+        <StyledButton onClick={()=> setExpanded(!expanded)}>
+            <StyledSvg $isVisible={expanded} data-genesis-element="ChevronBottomIcon" aria-hidden="true" role="presentation" xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="none" viewBox="0 0 20 20"><path fill="currentColor" d="M10 12.208a.864.864 0 0 1-.292-.052.64.64 0 0 1-.27-.198L5.75 8.292a.668.668 0 0 1-.177-.49.672.672 0 0 1 .198-.469c.153-.139.312-.205.48-.198a.72.72 0 0 1 .457.198L10 10.646l3.312-3.292a.574.574 0 0 1 .438-.208c.167 0 .326.07.48.208a.66.66 0 0 1 .208.479c0 .18-.07.334-.209.459l-3.667 3.666a.64.64 0 0 1-.27.198.864.864 0 0 1-.292.052Z"></path></StyledSvg>
         </StyledButton>
-    </TopicWrapper>
+      </TopicWrapper>
+      <OptionsWrapper $isVisible={expanded}>
         {filters.map((f: string) => <FilterWrapper>{f}</FilterWrapper>)}
+      </OptionsWrapper>
     </CardWrapper>
   )
 }
