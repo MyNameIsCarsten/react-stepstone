d994f7ad6ab67fe08397c6250f935e2b0669334c
Carsten Linder <Carsten@Carsten-Linder.de>
2023-11-22 13:37:46 +0100

    Like Button changes color, Form input fields of search are now controlled components

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -116,22 +116,54 @@ const OptionWrapper = styled.option`
 const Search: React.FC  = () => {
     const [searchIsFocused, setSearchIsFocused] = useState<boolean>(false);
     const [geoIsFocused, setGeoIsFocused] = useState<boolean>(false);
+    const [searchKeyword, setSearchKeyword] = useState<string>('');
+    const [searchLocation, setSearchLocation] = useState<string>('');
 
   return (
     <SearchbarsWrapper>
         <SearchbarWrapper className={searchIsFocused ? 'focused' : ''} onBlur={() => setSearchIsFocused(false)} onFocus={() => setSearchIsFocused(true)}>
             <StyledSvg aria-hidden="true" role="img" xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="none" viewBox="0 0 20 20" aria-labelledby="MagnifyingGlassIcon-69"><title id="MagnifyingGlassIcon-69">Magnifying glass icon</title><path fill="currentColor" d="M15.792 16.688L11.083 12a4.76 4.76 0 01-1.489.802 5.21 5.21 0 01-1.615.26c-1.43 0-2.646-.493-3.646-1.479-1-.986-1.5-2.194-1.5-3.625 0-1.417.5-2.628 1.5-3.635 1-1.007 2.215-1.511 3.646-1.511 1.417 0 2.618.5 3.604 1.5.986 1 1.479 2.215 1.479 3.646 0 .57-.083 1.122-.25 1.657a4.153 4.153 0 01-.791 1.447l4.708 4.73a.573.573 0 01.177.448.65.65 0 01-.198.448.647.647 0 01-.468.208.582.582 0 01-.448-.208zm-7.813-4.959c1.042 0 1.927-.368 2.656-1.104.73-.736 1.094-1.625 1.094-2.667 0-1.055-.361-1.955-1.083-2.698-.723-.743-1.612-1.114-2.667-1.114-1.07 0-1.972.371-2.708 1.114-.736.743-1.104 1.643-1.104 2.698 0 1.056.368 1.948 1.104 2.677.736.73 1.639 1.094 2.708 1.094z"></path></StyledSvg>
-            <SearchBarWrapper type="text" placeholder="(Jobtitel, Kompetenz oder Firmenname)"/>
-            <SvgButtonWrapper  $round={true}>
+            <SearchBarWrapper 
+                type="text" 
+                placeholder="(Jobtitel, Kompetenz oder Firmenname)"
+                value={searchKeyword}
+                onChange={(e) => setSearchKeyword(e.target.value)}
+            />
+            <SvgButtonWrapper  $round={true}
+                onClick={() => setSearchKeyword('')}
+            >
                 <svg aria-hidden="true" role="img" xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="none" viewBox="0 0 20 20" aria-labelledby="RemoveIcon-72"><title id="RemoveIcon-72">Remove icon</title><path fill="currentColor" d="M10 10.938l-4.167 4.166a.61.61 0 01-.448.188.645.645 0 01-.468-.209.645.645 0 01-.209-.468c0-.174.07-.33.209-.469L9.062 10 4.896 5.833a.657.657 0 01.021-.937.647.647 0 01.468-.208c.174 0 .33.07.469.208L10 9.062l4.167-4.166a.657.657 0 01.458-.198c.18-.007.34.06.479.198a.648.648 0 01.208.469c0 .173-.07.33-.208.468L10.938 10l4.166 4.167a.647.647 0 01.198.448.61.61 0 01-.198.468.646.646 0 01-.469.209.645.645 0 01-.468-.209L10 10.938z"></path></svg>
             </SvgButtonWrapper>
         </SearchbarWrapper>
 
         <SearchbarWrapper className={geoIsFocused ? 'focused' : ''} onBlur={() => setGeoIsFocused(false)} onFocus={() => setGeoIsFocused(true)}>
             <StyledSvg aria-hidden="true" role="img" xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="none" viewBox="0 0 20 20" aria-labelledby="MapMarkerIcon-81"><title id="MapMarkerIcon-81">Map marker icon</title><path fill="currentColor" d="M9.998 9.833c.417 0 .77-.146 1.062-.437.292-.292.438-.646.438-1.063 0-.417-.146-.77-.438-1.062a1.444 1.444 0 00-1.062-.438c-.417 0-.77.146-1.062.438a1.444 1.444 0 00-.438 1.062c0 .417.146.771.438 1.063.291.291.645.437 1.062.437zm0 6.542c1.708-1.514 2.99-2.955 3.844-4.323.854-1.368 1.281-2.552 1.281-3.552 0-1.583-.493-2.861-1.479-3.833-.986-.973-2.201-1.459-3.646-1.459-1.445 0-2.66.486-3.646 1.459-.986.972-1.479 2.25-1.479 3.833 0 1 .427 2.184 1.281 3.552.855 1.368 2.136 2.809 3.844 4.323zm0 1.458a.967.967 0 01-.604-.187c-1.972-1.805-3.44-3.462-4.406-4.969C4.023 11.17 3.54 9.778 3.54 8.5c0-2.028.646-3.639 1.937-4.833 1.292-1.195 2.799-1.792 4.521-1.792s3.229.597 4.521 1.792c1.291 1.194 1.937 2.805 1.937 4.833 0 1.278-.483 2.67-1.448 4.177-.965 1.507-2.434 3.164-4.406 4.969a.732.732 0 01-.281.146 1.266 1.266 0 01-.323.041z"></path></StyledSvg>
-            <SearchBarWrapper type="text" placeholder="(Ort oder 5-stellige PLZ)"/>
-            <SvgButtonWrapper>
-                <svg aria-hidden="true" role="img" xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="none" viewBox="0 0 20 20" aria-labelledby="RemoveIcon-72"><title id="RemoveIcon-72">Remove icon</title><path fill="currentColor" d="M10 10.938l-4.167 4.166a.61.61 0 01-.448.188.645.645 0 01-.468-.209.645.645 0 01-.209-.468c0-.174.07-.33.209-.469L9.062 10 4.896 5.833a.657.657 0 01.021-.937.647.647 0 01.468-.208c.174 0 .33.07.469.208L10 9.062l4.167-4.166a.657.657 0 01.458-.198c.18-.007.34.06.479.198a.648.648 0 01.208.469c0 .173-.07.33-.208.468L10.938 10l4.166 4.167a.647.647 0 01.198.448.61.61 0 01-.198.468.646.646 0 01-.469.209.645.645 0 01-.468-.209L10 10.938z"></path></svg>
+            <SearchBarWrapper 
+                type="text" 
+                placeholder="(Ort oder 5-stellige PLZ)"
+                value={searchLocation}
+                onChange={(e) => setSearchLocation(e.target.value)}
+            />
+            <SvgButtonWrapper
+                onClick={() => setSearchLocation('')}
+            >
+                <svg 
+                    aria-hidden="true" 
+                    role="img" 
+                    xmlns="http://www.w3.org/2000/svg" 
+                    width="100%" 
+                    height="100%" 
+                    fill="none" 
+                    viewBox="0 0 20 20" 
+                    aria-labelledby="RemoveIcon-72">
+                    <title 
+                        id="RemoveIcon-72">Remove icon
+                    </title>
+                    <path 
+                        fill="currentColor" 
+                        d="M10 10.938l-4.167 4.166a.61.61 0 01-.448.188.645.645 0 01-.468-.209.645.645 0 01-.209-.468c0-.174.07-.33.209-.469L9.062 10 4.896 5.833a.657.657 0 01.021-.937.647.647 0 01.468-.208c.174 0 .33.07.469.208L10 9.062l4.167-4.166a.657.657 0 01.458-.198c.18-.007.34.06.479.198a.648.648 0 01.208.469c0 .173-.07.33-.208.468L10.938 10l4.166 4.167a.647.647 0 01.198.448.61.61 0 01-.198.468.646.646 0 01-.469.209.645.645 0 01-.468-.209L10 10.938z"
+                    ></path>
+                </svg>
             </SvgButtonWrapper>
             <SvgButtonWrapper $width='5em' $bg={true} $round={true}>
                 <form>
